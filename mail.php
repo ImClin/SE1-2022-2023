19fee95a904f58dbfb905a75ce60e5137876b7a4
ImClin <colincanters@gmail.com>
2022-10-29 20:57:24 +0200

    mail function

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST["email"];
+    $problem = $_POST["problem"];
+    $message = $_POST["message"];
+    $to = "4797@laurenslyceum.nl";	
+
+    mail($to, $problem, $message, "From: $name <$email>");
+    echo "Uw bericht is vestuurd!";
+}
+?>
